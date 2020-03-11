bac487604e573f6d0e7d0b855a4d8acd64fb179a
Matuk <mateusmatuk@fai-mg.br>
2020-03-10 21:49:05 -0300

    Finish him

--- src/br/fai/projeto06/Main.java
+++ src/br/fai/projeto06/Main.java
@@ -12,12 +12,30 @@ public class Main {
 	private void Start() {
 
 		String[] Vrumvrum = new String[]{"civic", "gol", "uno", "palio"};
-	
-		for(int i=0;i<4;i++){
+		
+		for(int i=0;i<4;i++)
+		{
 			System.out.println(Vrumvrum[i]);
 		}
 		
+		Vrumvrum[1] = "l200";
 		
+		for(int i=0;i<4;i++)
+		{
+			System.out.println(Vrumvrum[i]);
+		}
+		int tamanho = Vrumvrum.length + 1;
+		String[] Vrumvrumvrum = new String[tamanho];
+		
+		for(int i=0;i<Vrumvrum.length;i++)
+		{
+			Vrumvrumvrum[i] = Vrumvrum[i];
+		}
+		Vrumvrumvrum[4] = "gol";
+		for(int i=0;i<tamanho;i++)
+		{
+			System.out.println(Vrumvrumvrum[i]);
+		}
 	}
 
 }
